2db52738271a5d3c252e95ee1ee29b5b6b3763f0
Miguel Flores <mike97@hotmail.es>
2017-05-05 14:32:35 -0600

    aca hago la creacion de los jugadores

---
+++ src/lab3_miguelflores/Lab3_miguelflores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_miguelflores;
+
+/**
+ *
+ * @author Miguel Flores
+ */
+public class Lab3_miguelflores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int n = 0;
+        
+    }
+
+}
